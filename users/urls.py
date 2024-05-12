31399fe99911a1e6a5d02aa84669b6f606675d01
Arindamghosh32 <138763107+Arindamghosh32@users.noreply.github.com>
2024-05-12 17:34:56 +0530

    Update urls.py

--- users/urls.py
+++ users/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns=[
     path('',views.all_user,name='all_user'),
-    path('dashboard/',views.chart_view,name='dash'),
+    path('dashboard/',views.chart_view,name='Home'),
     path('region/', views.region, name="region")
 ]
